2bd7ad6b1c1aeb9d2c0d739c8b290e24475940f3
carjeff <747938871@qq.comf>
2022-08-01 16:35:14 +0800

    mybatis学习 “

--- SSM/src/main/java/com/jason/mybatis/mapper/UserMapper.java
+++ SSM/src/main/java/com/jason/mybatis/mapper/UserMapper.java
@@ -1,5 +1,10 @@
 package com.jason.mybatis.mapper;
 
+import com.jason.mybatis.pojo.User;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Jason
  * @date 2022/7/30 19:39
@@ -7,5 +12,21 @@ package com.jason.mybatis.mapper;
  */
 public interface UserMapper {
     //添加用户
-    int insertUser();
+    int insertUser(User user);
+
+
+    //根据id查找用户
+    User getUserById();
+
+    //查询所有用户对象
+    List<User> getAllUsers();
+
+    //根据用户名查找用户
+    User getUserByUsername(String username);
+
+    //验证登录
+    User login(String username, String password);
+
+    //
+    User checkLoginByMap(Map<String,Object> map);
 }
--- SSM/src/test/java/MybatisTest.java
+++ SSM/src/test/java/MybatisTest.java
@@ -1,4 +1,5 @@
 import com.jason.mybatis.mapper.UserMapper;
+import com.jason.mybatis.pojo.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -7,6 +8,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * @author Jason
@@ -23,10 +25,33 @@ public class MybatisTest {
         SqlSession sqlSession = sqlSessionFactory.openSession();
         //获取mapper的代理实现类对象
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-        int i = mapper.insertUser();
+        int i = mapper.insertUser(new User());
         System.out.println("结果" + i);
         //提交事务
         sqlSession.commit();
         sqlSession.close();
+    }
+
+
+    @Test
+    public void testGetUserById() throws IOException {
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.getUserById();
+        System.out.println(user);
+    }
+
+    @Test
+    public void testGetAllUsers() throws IOException {
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> user = mapper.getAllUsers();
+        System.out.println(user);
     }
 }
